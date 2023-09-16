fd7ec536a82dcd6f6dc222b8602b3923dadbb2b5
Mmsshakil <softhandshakil@gmail.com>
2023-09-16 15:41:47 +0600

    double course can't added this work done and added the toast

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,25 @@ import { useState } from 'react'
 import './App.css'
 import Cards from './components/Cards/Cards'
 import Bookmarks from './components/Bookmarks/Bookmarks';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
 
   const [bookmarks, setBookmarks] = useState([]);
   const [credit, setCredit] = useState(0);
   const [price, setPrice] = useState(0);
+  const [remaining, setRemaining] = useState(20);
 
   const handleAddToBookmark = (card, newCredit, newPrice) => {
+    const a = [...bookmarks];
     const newBookmarks = [...bookmarks, card];
+
+    if (a.includes(card)) {
+      toast.warn("This course already added !");
+      return;
+    }
+
     setBookmarks(newBookmarks);
     handleCredit(newCredit);
     handlePrice(newPrice);
@@ -28,14 +38,20 @@ function App() {
   }
 
   return (
-    <div className='md:max-w-7xl md:mx-auto my-4 md:my-12'>
-      <h1 className='text-3xl font-bold text-center mb-4 md:mb-8'>Course Registration</h1>
-      <div className="flex flex-col lg:flex-row gap-6">
-        <Cards className="w-2/3" handleAddToBookmark={handleAddToBookmark} handleCredit={handleCredit} />
-        <Bookmarks className="w-1/3" bookmarks={bookmarks} credit={credit} price={price} />
+    <>
+      <div className='md:max-w-7xl md:mx-auto my-4 md:my-12'>
+        <h1 className='text-3xl font-bold text-center mb-4 md:mb-8'>Course Registration</h1>
+        <div className="flex flex-col lg:flex-row gap-6">
+          <Cards className="w-2/3" handleAddToBookmark={handleAddToBookmark} handleCredit={handleCredit} />
+          <Bookmarks className="w-1/3" bookmarks={bookmarks} credit={credit} price={price} />
+        </div>
       </div>
-    </div>
+      <ToastContainer />
+    </>
+
+
   )
+
 }
 
 export default App
